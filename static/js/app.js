dd61984856f94ef7b3e68c6d482c9b9abedff16e
gforce2332 <tcrew05@gmail.com>
2022-06-03 19:59:09 -0700

    minor edits

--- static/js/app.js
+++ static/js/app.js
@@ -30,43 +30,45 @@ var filters = {};
 function updateFilters() {
 
     // 4a. Save the element that was changed as a variable.
-    var inputElem = d3.select(this);
+    let changedElement = d3.select(this);
 
     // 4b. Save the value that was changed as a variable.
-    var inputValue = d3.select(this).property("value");
+    let elementValue = changedElement.property("value");
+    console.log(elementValue);
 
     // 4c. Save the id of the filter that was changed as a variable.
-    var inputId = d3.select(this).property("id");
+    let filterId = changedElement.attr("id");
+    console.log(filterId);
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
-    if (inputValue) {
-      filters[inputId] = inputValue;
+    if (elementValue) {
+      filters[filterId] = elementValue;
     }
     else {
-      delete filters[inputId];
-    };
+      delete filters[filterId];
+    }
   
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
   
-  };
+  }
   
   // 7. Use this function to filter the table when data is entered.
   function filterTable() {
   
     // 8. Set the filtered data to the tableData.
-    filteredData = tableData;
+    let filteredData = tableData;
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    Object.keys(filters).forEach((key) => {
-        filteredData = filteredData.filter(row => row[key] === filters[key]);
+    Object.entries(filters).forEach(([filterId, elementValue]) => {
+      filteredData = filteredData.filter(row => row[filterId] === elementValue);
     });
     
     // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData);
-  };
+  }
   
   // 2. Attach an event to listen for changes to each filter
   d3.selectAll("input").on("change", updateFilters);
